cc7df35f39e378dcf509f87943fc2659bff97f09
xyn <102333416+notxynn@users.noreply.github.com>
2024-04-28 19:37:55 -0500

    Update PFHandler.lua

--- PFHandler.lua
+++ PFHandler.lua
@@ -6,25 +6,16 @@ Channel.Event:Connect(function(Func,ID,Index,Value)
         local Passed, Statement = pcall(function()
             Drawings[ID]:Remove();
         end);
-        if not Passed then
-            print("Failed to remove drawing.");
-        end;
     elseif Func == "new" then
         local Passed, Statement = pcall(function()
             Drawings[ID] = Drawing.new(Index);
         end);
-        if not Passed then
-            print("Failed to create drawing.");
-        end;
     else
         local Type = Drawings[ID];
         if Type then
             local Passed, Statement = pcall(function()
                 Type[Index] = value;
             end);
-            if not Passed then
-                print("Failed to modify drawing.");
-            end;
         end;
     end;
 end);
