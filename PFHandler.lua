1bbef14916a604bd90fb8e83d96175fcaf3b7d26
xyn <102333416+notxynn@users.noreply.github.com>
2024-04-28 19:43:27 -0500

    Update PFHandler.lua

--- PFHandler.lua
+++ PFHandler.lua
@@ -2,13 +2,13 @@
 local Drawings = {};
 local Thing, Channel = create_comm_channel();
 Channel.Event:Connect(function(Func,ID,Index,Value)
-    if Func == "new" then
-        local Passed, Statement = pcall(function()
-            Drawings[ID] = Drawing.new(Index);
-        end);
-    elseif Func == "remove" then
-        local Passed, Statement = pcall(function()
-            Drawings[ID]:Remove();
+    if Func == "remove" then
+        pcall(function()
+            Drawings[ID]:Remove()
+        end)
+    elseif Func == "new" then
+        pcall(function()
+            Drawings[ID] = Drawing.new(Index)
         end)
     else
         local Type = Drawings[ID];
