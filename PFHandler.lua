19d02d317588d28e40169788e0ad7231b5aad99f
xyn <102333416+notxynn@users.noreply.github.com>
2024-04-28 19:56:36 -0500

    Update PFHandler.lua

--- PFHandler.lua
+++ PFHandler.lua
@@ -1,21 +1,21 @@
-local realDrawings = {}
-local newID, newChannel = create_comm_channel()
-newChannel.Event:Connect(function(method, drawingID, index, value)
-    if method == "remove" then
-        pcall(function()
-            realDrawings[drawingID]:Remove()
-        end)
-    elseif method == "new" then
-        pcall(function()
-            realDrawings[drawingID] = Drawing.new(index)
+-- // Pop a band 😊
+local Drawings = {};
+local Thing, Channel = create_comm_channel();
+Channel.Event:Connect(function(Func,ID,Index,Value)
+    if Func == "new" then
+        local Passed, Statement = pcall(function()
+            Drawings[ID] = Drawing.new(Index);
+        end);
+    elseif Func == "remove" then
+        local Passed, Statement = pcall(function()
+            Drawings[ID]:Remove();
         end)
     else
-        local shape = realDrawings[drawingID]
-
-        if shape then
-            pcall(function()
-                shape[index] = value
-            end)
-        end
-    end
-end)
+        local Type = Drawings[ID];
+        if Type then
+            local Passed, Statement = pcall(function()
+                Type[Index] = Value;
+            end);
+        end;
+    end;
+end);
